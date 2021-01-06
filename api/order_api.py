312f81249dc1e3504ba621a7f28b2b19c2d85343
Dexter F <52710349+dexterfhy@users.noreply.github.com>
2021-01-06 10:48:43 +0800

    Updated order api

--- api/order_api.py
+++ api/order_api.py
@@ -1,40 +1,39 @@
+import json
+
 from requests import request
-import constants
+
 from beans.user import User
+from constants import MENU_CODES_TO_OPTIONS
 from utils import default_if_blank
 
 
 # Calls an API to list orders for a user given his Telegram user id
 def list_orders(user: User):
-    # !!! Commented out first since API is not done, also returns dummy data
-    # return json.loads(__send_request('GET', get_orders_url(user), ()).text)
-    return [
-        {
-            "order_description": "2x Rye Sourdough Bread, 1x Teriyaki Chicken Burger, 5x, Minute Steak Frites",
-            "timestamp": "29th December 2020, 12 PM"
-        },
-        {
-            "order_description": "2x Spaghetti Aglio Olio, 1x Almond Crunch Cookie",
-            "timestamp": "29th December 2020, 5 PM"
-        }
-    ]
+    return list(map(__parse_order, json.loads(__send_request('GET', get_orders_url(user), ()).text)))
 
 
 # Calls an API to create a new order (with one or many menu items) for a user given his Telegram user id
 def create_order(user: User, order_items):
-    # !!! Commented out first since API is not done
-    # return __send_request('POST', get_orders_url(user), order_items)
-    return {}
+    return __send_request('POST', get_orders_url(user), order_items)
+
+
+# Parses the response from list orders endpoint into a list of order descriptions and timestamps
+def __parse_order(order):
+    return {
+        "order_description": ", ".join(
+            map(lambda item: "{}x {}".format(item['count'], MENU_CODES_TO_OPTIONS[item['code']]),
+                filter(lambda item: item['code'] in MENU_CODES_TO_OPTIONS, order['items']))),
+        "timestamp": order['timestamp']
+    }
 
 
 def get_orders_url(user: User):
-    return "https://api.com/sg/ninjacafe/%s/orders".format(default_if_blank(user.id, ''))
+    return "https://api-qa.ninjavan.co/global/ninjacafe/orders/{}".format(default_if_blank(user.id, ''))
 
 
 def __send_request(method, url, body):
     headers = {
-        "Connection": "keep-alive",
-        "Authorization": "Bearer " + constants.INTERNAL_ACCESS_TOKEN
+        "Connection": "keep-alive"
     }
 
     response = request(method, headers=headers, url=url, data=body)
