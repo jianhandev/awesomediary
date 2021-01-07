960ffab97f3e181329aedb9a7e653c86fe318036
Dexter Fong <dexterfonghy@gmail.com>
2021-01-07 11:08:48 +0800

    Added more default code

--- beans/session.py
+++ beans/session.py
@@ -1,4 +1,12 @@
-# FILL IN CODE
-# Remove pass placeholder
 class Session:
-    pass
+    def __init__(self, sid, is_new):
+        self.__id = sid
+        self.__is_new = is_new
+
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def is_new(self):
+        return self.__is_new
--- cache.py
+++ cache.py
@@ -15,25 +15,42 @@ def get_current_session(user: User):
 
 # Returns the Dictionary of items (and their counts) for the user in the current session order, or an empty Dict if none
 def get_current_order(user: User, session_id):
-    # FILL IN CODE
-    return
+    orders_key = __current_orders_key(user, session_id)
+    orders = cache.get(orders_key)
+
+    if orders is not None and len(orders) > 0:
+        return orders
+    else:
+        cache.set(orders_key, {})
+        return {}
 
 
 # Adds a list of Item objects to the user's current order
 def add_to_order(user: User, session_id, items):
-    # FILL IN CODE
-    return
+    orders_key = __current_orders_key(user, session_id)
+    orders = get_current_order(user, session_id)
+
+    for item in items:
+        orders = __add_item_to_orders(orders, item)
+
+    cache.set(orders_key, orders)
+
+    return orders
 
 
 # Clears the list of Item objects for the user's current order
 def clear_from_order(user: User, session_id):
-    # FILL IN CODE
-    return
+    orders_key = __current_orders_key(user, session_id)
+    cache.delete(orders_key)
 
 
 def __add_item_to_orders(orders, item: Item):
-    # FILL IN CODE
-    return
+    if item.name in orders:
+        orders[item.name] = orders[item.name] + item.count
+    else:
+        orders[item.name] = item.count
+
+    return orders
 
 
 def __session_key(user: User):
@@ -42,5 +59,4 @@ def __session_key(user: User):
 
 
 def __current_orders_key(user: User, session_id):
-    # FILL IN CODE
-    return
+    return "orders_{}_{}".format(default_if_blank(user.id, ''), session_id)
